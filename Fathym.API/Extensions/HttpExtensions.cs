8d094d8d026a931b3c1421969c7b2bd3ccb76a58
Michael Gearhardt <michael.gearhardt@5280software.com>
2018-02-04 09:51:56 -0700

    Iniitial structures

---
+++ Fathym.API/Extensions/HttpExtensions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Net.Http
+{
+	public static class HttpExtensions
+	{
+		public static async Task<HttpResponseMessage> PatchAsync(this HttpClient client, string requestUri, HttpContent content)
+		{
+			return await PatchAsync(client, new Uri(client.BaseAddress + requestUri), content);
+		}
+
+		public static async Task<HttpResponseMessage> PatchAsync(this HttpClient client, Uri requestUri, HttpContent content)
+		{
+			var request = new HttpRequestMessage
+			{
+				Method = new HttpMethod("PATCH"),
+				RequestUri = requestUri,
+				Content = content
+			};
+
+			return await client.SendAsync(request);
+		}
+
+		public static async Task<HttpResponseMessage> PatchAsJsonAsync<T>(this HttpClient client,
+			string requestUri, T value)
+		{
+			return await PatchAsJsonAsync<T>(client, new Uri(client.BaseAddress + requestUri), value);
+		}
+
+		public static async Task<HttpResponseMessage> PatchAsJsonAsync<T>(this HttpClient client,
+			Uri requestUri, T value)
+		{
+			var content = new ObjectContent<T>(value, new JsonMediaTypeFormatter(), "application/json");
+
+			return await client.PatchAsync(requestUri, content);
+		}
+		
+		public static async Task<T> ReadAsJSONAsync<T>(this HttpContent content)
+		{
+			var str = await content.ReadAsStringAsync();
+
+			return str.FromJSON<T>();
+		}
+
+	}
+}
---
+++ Fathym/Extensions/StringExtensions.cs
@@ -0,0 +1,76 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System
+{
+	public static class StringExtensions
+	{
+		public static string Base64Decode(this string value)
+		{
+			return Encoding.ASCII.GetString(Convert.FromBase64String(value));
+		}
+
+		public static string Base64Encode(this string value)
+		{
+			return Convert.ToBase64String(Encoding.ASCII.GetBytes(value));
+		}
+		
+		public static dynamic FromJSON(this string value)
+		{
+			if (value.IsNullOrEmpty())
+				return null;
+
+			return JsonConvert.DeserializeObject(value);
+		}
+
+		public static T FromJSON<T>(this string value, JsonSerializerSettings serializationSettings = null)
+		{
+			if (value.IsNullOrEmpty())
+				return default(T);
+
+			return JsonConvert.DeserializeObject<T>(value, serializationSettings);
+		}
+
+		public static bool IsNullOrEmpty(this string value)
+		{
+			return String.IsNullOrEmpty(value);
+		}
+		
+		public static T ToEnum<T>(this string value)
+		{
+			return (T)ToEnum(value, typeof(T));
+		}
+
+		public static object ToEnum(this string value, Type type)
+		{
+			return Enum.Parse(type, value);
+		}
+
+		public static string ToMD5Hash(this string value, bool lowerCaseHash = false)
+		{
+			var md5 = System.Security.Cryptography.MD5.Create();
+
+			var inputBytes = System.Text.Encoding.ASCII.GetBytes(value);
+
+			var hash = md5.ComputeHash(inputBytes);
+
+			var sb = new StringBuilder();
+
+			for (var i = 0; i < hash.Length; i++)
+				sb.Append(hash[i].ToString("X2"));
+
+			var md5Hash = sb.ToString();
+
+			return lowerCaseHash ? md5Hash.ToLower() : md5Hash.ToUpper();
+		}
+		
+		public static string URLEncode(this string value)
+		{
+			return System.Text.Encodings.Web.UrlEncoder.Default.Encode(value);
+		}
+	}
+}
