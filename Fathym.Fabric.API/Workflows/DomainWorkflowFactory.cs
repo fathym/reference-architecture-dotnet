66690f40ea5512659cecb680f810a303a084adb9
Michael Gearhardt <michael.gearhardt@fathym.com>
2018-07-16 10:20:56 -0600

    New workflow factory

---
+++ Fathym.Fabric.API/Workflows/DomainWorkflowFactory.cs
@@ -0,0 +1,39 @@
+﻿using Fathym.Fabric.Runtime.Adapters;
+using Microsoft.ServiceFabric.Actors;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fathym.Fabric.API.Workflows
+{
+	public abstract class DomainWorkflowFactory<TWorkflow>
+		where TWorkflow : DomainWorkflowActor, IActor
+	{
+		#region Fields
+		protected readonly IFabricAdapter fabricAdapter;
+		#endregion
+
+		#region Constructors
+		public DomainWorkflowFactory(IFabricAdapter fabricAdapter)
+		{
+			this.fabricAdapter = fabricAdapter;
+		}
+		#endregion
+
+		#region API Methods
+		public virtual TWorkflow Build(string primaryApiKey, bool makeUnique = false)
+		{
+			var actorId = buildActorId(primaryApiKey);
+
+			if (makeUnique)
+				actorId += $"|{Guid.NewGuid()}";
+
+			return fabricAdapter.BuildActorProxy<TWorkflow>(actorId);
+		}
+		#endregion
+
+		#region Helpers
+		protected abstract string buildActorId(string primaryApiKey);
+		#endregion
+	}
+}
--- Fathym.Presentation/Web/ViewTagHelper.cs
+++ Fathym.Presentation/Web/ViewTagHelper.cs
@@ -71,9 +71,7 @@ namespace Fathym.Presentation.Web
 					});
 			}
 			else
-			{
-				output.PostElement.AppendHtml("<span>Unable to locate application files</span><script>setInterval(() => { location.href = location.href; }, 5000);</script>");
-			}
+				output.PostElement.AppendHtml("<span>Unable to locate application files</span><script>setInterval(() => { location.href = location.href; }, 1750);</script>");
 		}
 		#endregion
 
