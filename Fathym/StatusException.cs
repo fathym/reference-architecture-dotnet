b0fd50d2ab0a772a59917cd3fc9b083c4e174907
mcgear <michael.gearhardt@gmail.com>
2020-03-30 15:31:16 -0600

    StatusException

---
+++ Fathym/StatusException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Fathym
+{
+	public class StatusException : Exception
+	{
+		#region Properties
+		public virtual Status Status { get; protected set; }
+		#endregion
+
+		#region Constructors
+		public StatusException(Status status)
+			: base(status.Message)
+		{
+			Status = status;
+		}
+
+		public StatusException(Status status, Exception ex)
+			: base(status.Message, ex)
+		{
+			Status = status;
+		}
+		#endregion
+	}
+}
