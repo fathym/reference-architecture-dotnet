fcdf19fa3d75e0f1f8a900e5b948ced455ab4d6a
Michael Gearhardt <michael.gearhardt@fathym.com>
2022-07-26 11:24:07 -0600

    Change namespace

--- Fathym.Presentation.MVC/Controllers/FathymController.cs
+++ Fathym.Presentation.MVC/Controllers/FathymController.cs
@@ -7,41 +7,41 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Fathym.Presentation.MVC
+namespace Fathym.Presentation.MVC.Controllers
 {
-	public class FathymController : Controller
-	{
-		#region Fields
-		protected string currentAction;
+    public class FathymController : Controller
+    {
+        #region Fields
+        protected string currentAction;
 
-		protected string currentController;
-		#endregion
+        protected string currentController;
+        #endregion
 
-		#region Constructors
-		public FathymController()
-		{ }
-		#endregion
+        #region Constructors
+        public FathymController()
+        { }
+        #endregion
 
-		#region API Methods
-		public virtual async Task<IActionResult> Index()
-		{
-			return View();
-		}
-		#endregion
+        #region API Methods
+        public virtual async Task<IActionResult> Index()
+        {
+            return View();
+        }
+        #endregion
 
-		#region Life Cycle
-		public override void OnActionExecuting(ActionExecutingContext context)
-		{
-			ViewBag.CurrentAction = currentAction = context.RouteData.Values["action"].ToString();
+        #region Life Cycle
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            ViewBag.CurrentAction = currentAction = context.RouteData.Values["action"].ToString();
 
-			ViewBag.CurrentController = currentController = context.RouteData.Values["controller"].ToString();
+            ViewBag.CurrentController = currentController = context.RouteData.Values["controller"].ToString();
 
-			ViewBag.UXRelease = (Assembly.GetEntryAssembly() ?? GetType().Assembly)
-						.GetCustomAttribute<AssemblyInformationalVersionAttribute>()
-						.InformationalVersion;
+            ViewBag.UXRelease = (Assembly.GetEntryAssembly() ?? GetType().Assembly)
+                        .GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+                        .InformationalVersion;
 
-			base.OnActionExecuting(context);
-		}
-		#endregion
-	}
+            base.OnActionExecuting(context);
+        }
+        #endregion
+    }
 }
